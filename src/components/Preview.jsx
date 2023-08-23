627545d60ed0d26392485fe6205dc59a0c953a47
sarantsevd <sarantsevd@terralink-global.com>
2023-08-23 14:41:01 +0300

    Preview content from task object

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,9 +1,12 @@
 import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
 
 function Preview({ content }) {
-    const docs = [{ uri: content }];
-
-    return <DocViewer documents={docs} pluginRenderers={DocViewerRenderers} />;
+  const docs = [{ uri: content }];
+  return content ? (
+    <DocViewer documents={docs} pluginRenderers={DocViewerRenderers} />
+  ) : (
+    <div>Файл не найден</div>
+  );
 }
 
 export default Preview;
