69d91c6ec197b876de42a12f5c10dffa4f58bd6b
sarantsevd <sarantsevd@terralink-global.com>
2023-08-23 14:13:43 +0300

    Add Header story

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,27 @@
-import React, { Component } from "react";
-import Sort from "./UI/Sort";
+import React, { Component } from 'react';
+import Sort from './UI/Sort';
 
 class Header extends Component {
-    render() {
-        return (
-            <>
-                <nav
-                    className="navbar navbar-light"
-                    style={{
-                        backgroundColor: "#e3f2fd",
-                        paddingLeft: "15px",
-                        paddingRight: "15px",
-                    }}
-                >
-                    <span className="navbar-brand mb-0">
-                        <h1>Задачи</h1>
-                    </span>
-                    <Sort sortItemClick={this.props.sortItemClick} />
-                </nav>
-            </>
-        );
-    }
+  render() {
+    const { sortItemClick, backgroundColor } = this.props;
+    return (
+      <>
+        <nav
+          className="navbar navbar-light"
+          style={{
+            backgroundColor: backgroundColor ? backgroundColor : '#e3f2fd',
+            paddingLeft: '15px',
+            paddingRight: '15px',
+          }}
+        >
+          <span className="navbar-brand mb-0">
+            <h1>Задачи</h1>
+          </span>
+          <Sort sortItemClick={sortItemClick} />
+        </nav>
+      </>
+    );
+  }
 }
 
 export default Header;
---
+++ src/stories/Header.stories.js
@@ -0,0 +1,20 @@
+import Header from '../components/Header';
+
+export default {
+  title: 'Inbox/Header',
+  component: Header,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen',
+  },
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+export const Default = {
+  args: { sortItemClick: () => {} },
+};
