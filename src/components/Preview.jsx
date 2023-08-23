aed7975e50f185b75d558b8f9f44dffd0690c6e9
sarantsevd <sarantsevd@terralink-global.com>
2023-08-23 15:16:01 +0300

    Preview Header adopted

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -3,7 +3,27 @@ import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
 function Preview({ content }) {
   const docs = [{ uri: content }];
   return content ? (
-    <DocViewer documents={docs} pluginRenderers={DocViewerRenderers} />
+    <DocViewer
+      documents={docs}
+      pluginRenderers={DocViewerRenderers}
+      theme={{
+        primary: "#5296d8",
+        secondary: "#ffffff",
+        // tertiary: "#5296d899",
+        tertiary: "rgba(82, 150, 216, 1)",
+        textPrimary: "#ffffff",
+        textSecondary: "#5296d8",
+        textTertiary: "#00000099",
+        disableThemeScrollbar: false,
+      }}
+      config={{
+        header: {
+          disableHeader: true,
+          disableFileName: false,
+          retainURLParams: false,
+        },
+      }}
+    />
   ) : (
     <div>Файл не найден</div>
   );
