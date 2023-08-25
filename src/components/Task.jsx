9f91c84d522ccb11093603d6c9b53466d0985592
sync <mick@vikingkong.xyz>
2023-08-25 18:13:42 +0300

    Style the Task component

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,22 +2,70 @@ import "./Task.css";
 import { useDispatch } from "react-redux";
 import { selectTask } from "../actions";
 
+const months = [
+  "Янв",
+  "Фев",
+  "Мар",
+  "Апр",
+  "Май",
+  "Июн",
+  "Июл",
+  "Авг",
+  "Сен",
+  "Окт",
+  "Ноя",
+  "Дек",
+];
+
+const getDate = (timestamp) => {
+  const currentDate = new Date(timestamp);
+  return { month: months[currentDate.getMonth()], date: currentDate.getDate() };
+};
+
+const colors = { Ok: "black", Warn: "orange", Error: "red" };
+
 function Task(props) {
-    const dispatch = useDispatch();
-    let classesString = "col task alert";
-    classesString += props.active ? " alert-secondary" : " alert-light";
-    return (
+  const date = getDate(props.date);
+  const dispatch = useDispatch();
+  const selectionWidth = props.active ? "3" : "0";
+  const taskColor = colors[props.level];
+  console.log(taskColor);
+  let styles = {
+    borderLeft: `${selectionWidth}px solid green`,
+    marginLeft: "5px",
+  };
+  return (
+    <div className="d-flex task" style={styles}>
+      <div
+        className="col"
+        role="alert"
+        onClick={() => dispatch(selectTask(props.id))}
+      >
+        <small style={{ color: taskColor }}>{props.stage}</small>
+        <h4 style={{ color: taskColor }}>{props.title}</h4>
+        <small className="text-muted">Автор: {props.author}</small>
+        <p>{props.body}</p>
+      </div>
+      <div>
         <div
-            className={classesString}
-            role="alert"
-            onClick={() => dispatch(selectTask(props.id))}
+          className="row h-50"
+          style={{ marginTop: "20px", textAlign: "center" }}
         >
-            <small>{props.stage}</small>
-            <h4>{props.title}</h4>
-            <small>Автор: {props.author}</small>
-            <p>{props.body}</p>
+          <div className="col-sm-8 align-items-center x-auto my-auto">
+            <h3 style={{ color: taskColor }}>{date.month}</h3>
+          </div>
         </div>
-    );
+        <div className="row">
+          <div
+            className="col-sm-8 align-items-center mx-auto my-auto"
+            style={{ textAlign: "center" }}
+          >
+            <h3 style={{ color: taskColor }}>{date.date}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Task;
