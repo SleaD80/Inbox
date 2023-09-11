b7ecb5aeb58c1562288ad19b195bf5bf23ed4230
sarantsevd <sarantsevd@terralink-global.com>
2023-09-11 16:32:50 +0300

    Apply bootswatch

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { legacy_createStore as createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootswatch/dist/materia/bootstrap.min.css';
+import './index.css';
+import App from './App';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
 const store = createStore(rootReducer, {}, applyMiddleware(thunk));
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
