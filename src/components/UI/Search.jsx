9039a42b1a485df0dc5ae4ebbc824538250f9b0e
sarantsevd <sarantsevd@terralink-global.com>
2023-09-11 14:54:38 +0300

    Stylish search component

--- src/components/UI/Search.jsx
+++ src/components/UI/Search.jsx
@@ -1,22 +1,18 @@
 import { useDispatch } from 'react-redux';
 import { search } from '../../actions';
+import './Search.css';
 
 function Search() {
   const dispatch = useDispatch();
 
   return (
-    <div className="input-group" style={{ padding: '15px' }}>
+    <div className="task-container">
       <input
-        type="text"
+        type="search"
         className="form-control"
         placeholder="Поиск"
-        aria-label=""
-        aria-describedby="basic-addon1"
         onChange={(e) => dispatch(search(e.target.value))}
       />
-      <div className="input-group-append">
-        <span className="input-group-text">Search</span>
-      </div>
     </div>
   );
 }
