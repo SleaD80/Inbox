c2563fc0ae954fc42c5adb44754e097f52847908
sync <chernovm@terralink-global.com>
2023-09-19 13:09:31 +0300

    Return to switching themes via changing href in link

--- src/components/ThemeProvider.jsx
+++ src/components/ThemeProvider.jsx
@@ -3,49 +3,33 @@ import { useSelector, useDispatch } from 'react-redux';
 import { loadStoredTheme } from '../actions';
 
 function ThemeProvider(props) {
-  //const themes = {
-  //slate: 'tasks/slate/bootstrap.min.css',
-  //cerulean: 'tasks/cerulean/bootstrap.min.css',
-  //materia: 'tasks/materia/bootstrap.min.css',
-  //};
+  const themes = {
+    slate: 'tasks/slate/bootstrap.min.css',
+    cerulean: 'tasks/cerulean/bootstrap.min.css',
+    materia: 'tasks/materia/bootstrap.min.css',
+  };
 
   let currentTheme = useSelector((store) => store.theme);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const styles = Array.from(document.querySelectorAll('style'));
-    let bootswatchStyles = styles.filter((item) =>
-      /Bootswatch/.test(item.innerText)
-    );
-    bootswatchStyles.forEach((item) => (item.disabled = true));
-    if (bootswatchStyles.length === 0) {
+    let themeSheet = document.getElementById('themeSheet');
+    if (!themeSheet) {
       const storedTheme = localStorage.getItem('theme');
       if (storedTheme) {
         currentTheme = storedTheme;
         dispatch(loadStoredTheme(currentTheme));
       }
-    }
-    bootswatchStyles.forEach((item) => (item.disabled = true));
-    let alreadyApplied = bootswatchStyles.find((item) =>
-      new RegExp('Theme: ' + currentTheme).test(item.innerText + '\n')
-    );
-    if (alreadyApplied) {
-      alreadyApplied.disabled = false;
+      themeSheet = document.createElement('link');
+      themeSheet.id = 'themeSheet';
+      themeSheet.href = themes[currentTheme];
+      themeSheet.rel = 'stylesheet';
+      themeSheet.type = 'text/css';
+      document.head.appendChild(themeSheet);
     } else {
-      require(`bootswatch/dist/${currentTheme}/bootstrap.min.css`);
+      themeSheet.href = themes[currentTheme];
     }
     localStorage.setItem('theme', currentTheme);
-    //let themeSheet = document.getElementById('themeSheet');
-    //if (!themeSheet) {
-    //themeSheet = document.createElement('link');
-    //themeSheet.id = 'themeSheet';
-    //themeSheet.href = themes[currentTheme];
-    //themeSheet.rel = 'stylesheet';
-    //themeSheet.type = 'text/css';
-    //document.head.appendChild(themeSheet);
-    //} else {
-    //themeSheet.href = themes[currentTheme];
-    //}
   });
 
   return props.children;
